e28f7747936cafaa859e2ab1680b17c33b25023e
雷日锦 <362606856@qq.com>
2018-07-09 21:03:19 +0800

    优化颜色工具栏配置
    优化颜色工具栏配置

--- skyeditor.js
+++ skyeditor.js
@@ -1,8 +1,11 @@
-var skyEl;
+
 var skyeditor = {};
+skyeditor.isfirst=true;
 skyeditor.root = "http://" + document.domain + "/plugin/skyeditor/";
-skyeditor.uploadUrl = "/shop.php?m=upload&a=uploadTao";
+skyeditor.uploadUrl = "/shop.php?m=upload&a=base64";
 skyeditor.uploadVideo = "/shop.php?m=upload&a=UploadVideo";
+skyeditor.colors=['#1ebdc0','#f8375b','#fd8f43','#a4ce3b','#35dab6','#199de1','#9581f3','#333','#8f8f94'];
+skyeditor.html='<div style="height: 50px;"></div><div class="sky-editor-loading">上传中...</div><div class="sky-editor">	<div class="sky-editor-colors">			</div>	<div class="sky-editor-emojis">			</div>	<div class="sky-editor-tools">		</div>	<input type="file" id="sky-editor-file" name="upimg" multiple="multiple" style="display: none;" />	<input type="file" id="sky-editor-file-video" name="upimg" style="display: none;" /></div>';
 skyeditor.skyUpload = function (upid, url, success, error, uploadProgress) {
     var vFD = new FormData();
     var f = document.getElementById(upid).files;
@@ -22,20 +25,64 @@ skyeditor.skyUpload = function (upid, url, success, error, uploadProgress) {
     }
 }
 skyeditor.init = function () {
-    $("<link>")
+   	if(skyeditor.isfirst){ 
+    	$("<link>")
         .attr({
             rel: "stylesheet",
             type: "text/css",
             href: skyeditor.root + "skyeditor.css"
         }).appendTo("head");
-    $.get(skyeditor.root + "skyeditor.html", function (data) {
-        $("body").append(data);
-        skyEmojis();
-    })
+		$("body").append(skyeditor.html);
+		skyeditor.isfirst=false;
+	} 
+    skyeditor.skyEmojis();   
+	skyeditor.setColors();
+    skyeditor.setTools();
+    
 
 }
+ 
+skyeditor.setColors=function(){
+	var html='';	
+	for(var i in skyeditor.colors){
+		html=html+'<div class="sky-editor-excute" style="background-color:'+skyeditor.colors[i]+';" data-role="ForeColor"></div>';
+	}
+	$(".sky-editor-colors").html(html);
+}
 
-function skyEmojis() {
+skyeditor.tools=["h3","pic","color","B","video","emoji","center"];
+skyeditor.setTools=function(){
+	var html='';
+	 
+	for(var i in skyeditor.tools){
+		switch(skyeditor.tools[i]){
+			case "h3":
+				html=html+'<div class="sky-editor-tools-item sky-editor-excute"  data-role="h3">h3</div>';
+				break;
+			case "pic":
+				html=html+'<div class="sky-editor-tools-item sky-editor-img"><i class="skyeditor-iconfont skyeditor-icon-pic"></i> </div>'
+				break;
+			case "color":
+				html=html+'<div class="sky-editor-tools-item sky-editor-colors-toggle">&nbsp; <span class="sky-editor-color"></span> </div>';
+				break;
+			case "B":
+				html=html+'<div class="sky-editor-tools-item sky-editor-excute" data-role="bold">B</div>';
+				break;
+			case "video":
+				html=html+'<div class="sky-editor-tools-item sky-editor-video"><i class="skyeditor-iconfont skyeditor-icon-video"></i> </div>';
+				break;
+			case "emoji":
+				html=html+'<div class="sky-editor-tools-item sky-editor-emoji-toggle"><i class="skyeditor-iconfont skyeditor-icon-emoji"></i></div>';
+				break;
+			case "center":
+				html=html+'<div class="sky-editor-tools-item sky-editor-excute" data-role="center"><i class="skyeditor-iconfont skyeditor-icon-centor"></i></div>';
+				break;
+		}
+	}
+	$(".sky-editor-tools").html(html);
+}
+
+skyeditor.skyEmojis=function() {
     var html = '';
     for (var i = 0; i < 84; i++) {
         var pos = i * 36 - i;
@@ -45,7 +92,7 @@ function skyEmojis() {
 }
 $(function () {
     skyeditor.init();
-    skyEl = $(".sky-editor-content");
+    skyeditor.skyEl = $(".sky-editor-content");
 
     document.documentElement.addEventListener('touchstart', function (event) {
         if (event.touches.length > 1) {
@@ -63,7 +110,7 @@ $(function () {
         } else {
             indexTag = "j_000" + index;
         }
-        skyEl.append('<img src="' + skyeditor.root + 'images/jx2/' + indexTag + '.gif" />');
+        skyeditor.skyEl.append('<img src="' + skyeditor.root + 'images/jx2/' + indexTag + '.gif" />');
         $(".sky-editor-emojis").hide();
     })
     $(document).on("click", ".sky-editor-colors-toggle", function () {
@@ -76,16 +123,17 @@ $(function () {
         $("#sky-editor-file-video").click();
     })
     $(document).on("change", "#sky-editor-file-video", function () {
+    	$(".sky-editor-loading").show();
         skyeditor.skyUpload("sky-editor-file-video", skyeditor.uploadVideo, function (e) {
-            console.log(e.target.responseText);
             var res = JSON.parse(e.target.responseText);
-
+			$(".sky-editor-loading").hide();
             if (res.error) {
                 mui.toast(res.error);
             } else {
-                var html = '<video controls  style="max-width:100%;" src="' + res.trueimgurl +
-                    '"></video>';
-                skyEl.append(html);
+                var html = '&nbsp; &nbsp;<video controls  style="max-width:100%;" src="' + res.trueimgurl +
+                    '"></video>&nbsp; &nbsp;</br>';
+                skyeditor.skyEl.append(html);
+                
             }
         })
     })
@@ -97,13 +145,13 @@ $(function () {
                 width: 1024
             })
             .then(function (rst) {
-                $.post("/shop.php?m=upload&a=base64", {
+                $.post(skyeditor.uploadUrl, {
                     content: rst.base64
                 }, function (data) {
 					$(".sky-editor-loading").hide();
                     if (data.error == 0) {
                         var html = '<img src="' + data.trueimgurl + '"> &nbsp;';
-                        skyEl.append(html);
+                        skyeditor.skyEl.append(html);
                     }
 					
                 }, "json");
@@ -114,24 +162,23 @@ $(function () {
         if ($(".sky-editor-content div").length == 0) {
             $(".sky-editor-content").prepend('<div style="color:#555;">&nbsp;</div>')
         }
-        skyEl = $(this).find("div").eq(0);
+        skyeditor.skyEl = $(this).find("div").eq(0);
 
     })
     $(document).on("click", ".sky-editor-content div", function (e) {
         e.stopPropagation();
-        skyEl = $(this);
+        skyeditor.skyEl = $(this);
         var color = $(".sky-editor-color").css("background-color");
         if ($(this).css("color") != color) {
             $(this).css("color", color);
         }
     })
     $(document).on("keyup", ".sky-editor-content", function (event) {
-        console.log(event.keyCode);
+        
         if (event.keyCode == "13"){     	 
         	setTimeout(function(){
-        		if(skyEl.next().length>0){
-        			skyEl = skyEl.next();
-        			console.log(skyEl);
+        		if(skyeditor.skyEl.next().length>0){
+        			skyeditor.skyEl = skyeditor.skyEl.next();
         		}	
         	},60) 	
         	return false;
@@ -147,13 +194,13 @@ $(function () {
 
             case 'h3':
                 if ($(this).hasClass("active")) {
-                    skyEl.css({
+                    skyeditor.skyEl.css({
                         fontSize: "16px",
                         fontWeight: "600",
                         "padding": "8px 0px"
                     });
                 } else {
-                    skyEl.css({
+                    skyeditor.skyEl.css({
                         fontSize: "14px",
                         fontWeight: "400",
                         "padding": "0"
@@ -164,26 +211,26 @@ $(function () {
             case "bold":
 
                 if ($(this).hasClass("active")) {
-                    skyEl.css("font-weight", "bolder");
+                    skyeditor.skyEl.css("font-weight", "bolder");
                 } else {
-                    skyEl.css("font-weight", "inherit");
+                    skyeditor.skyEl.css("font-weight", "inherit");
                 }
 
                 break;
             case "center":
 
                 if ($(this).hasClass("active")) {
-                    skyEl.css("text-align", "center");
+                    skyeditor.skyEl.css("text-align", "center");
                 } else {
-                    skyEl.css("text-align", "left");
+                    skyeditor.skyEl.css("text-align", "left");
                 }
 
                 break;
             case "ForeColor":
                 var color = $(this).css("background-color");
                 $(".sky-editor-color").css("background-color", color);
-                console.log(skyEl.html());
-                skyEl.css("color", color);
+                console.log(skyeditor.skyEl.html());
+                skyeditor.skyEl.css("color", color);
                 $(".sky-editor-colors").hide();
                 break;
 
