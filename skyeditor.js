df2afec29a01f3f257991e3a16aca6a2b579461d
雷日锦 <362606856@qq.com>
2018-07-09 17:48:27 +0800

    修复换行问题
    修复换行问题

--- skyeditor.js
+++ skyeditor.js
@@ -1,39 +1,40 @@
-var skyEL;
+var skyEl;
 var skyeditor = {};
 skyeditor.root = "http://" + document.domain + "/plugin/skyeditor/";
 skyeditor.uploadUrl = "/shop.php?m=upload&a=uploadTao";
-skyeditor.uploadVideo="/shop.php?m=upload&a=UploadVideo";
-skyeditor.skyUpload=function(upid,url,success,error,uploadProgress)
-{
-		 var vFD = new FormData();
-		 var f= document.getElementById(upid).files;
-		 $("#"+upid+"loading").show();
-		 for(var i=0;i<f.length;i++){ 
-		vFD.append('upimg', document.getElementById(upid).files[i]);
-		// create XMLHttpRequest object, adding few event listeners, and POSTing our data
-		var oXHR = new XMLHttpRequest();        
-		oXHR.addEventListener('load', success, false);
-		oXHR.addEventListener('error', error, false);
-		if(uploadProgress!=undefined){
-			oXHR.upload.addEventListener("progress", uploadProgress, false);
-		}
-		oXHR.open('POST',url);
-		oXHR.send(vFD);
-	
-		 }
+skyeditor.uploadVideo = "/shop.php?m=upload&a=UploadVideo";
+skyeditor.skyUpload = function (upid, url, success, error, uploadProgress) {
+    var vFD = new FormData();
+    var f = document.getElementById(upid).files;
+    $("#" + upid + "loading").show();
+    for (var i = 0; i < f.length; i++) {
+        vFD.append('upimg', document.getElementById(upid).files[i]);
+        // create XMLHttpRequest object, adding few event listeners, and POSTing our data
+        var oXHR = new XMLHttpRequest();
+        oXHR.addEventListener('load', success, false);
+        oXHR.addEventListener('error', error, false);
+        if (uploadProgress != undefined) {
+            oXHR.upload.addEventListener("progress", uploadProgress, false);
+        }
+        oXHR.open('POST', url);
+        oXHR.send(vFD);
+
+    }
 }
-skyeditor.init=function(){
-	$("<link>")
-	.attr({ rel: "stylesheet",
-		type: "text/css",
-		href: skyeditor.root+"skyeditor.css"
-	}).appendTo("head");
-	$.get(skyeditor.root+"skyeditor.html",function(data){
-		$("body").append(data);
-		skyEmojis();
-	})
-	
+skyeditor.init = function () {
+    $("<link>")
+        .attr({
+            rel: "stylesheet",
+            type: "text/css",
+            href: skyeditor.root + "skyeditor.css"
+        }).appendTo("head");
+    $.get(skyeditor.root + "skyeditor.html", function (data) {
+        $("body").append(data);
+        skyEmojis();
+    })
+
 }
+
 function skyEmojis() {
     var html = '';
     for (var i = 0; i < 84; i++) {
@@ -43,9 +44,9 @@ function skyEmojis() {
     $(".sky-editor-emojis").html(html);
 }
 $(function () {
-	skyeditor.init();
+    skyeditor.init();
     skyEl = $(".sky-editor-content");
-    
+
     document.documentElement.addEventListener('touchstart', function (event) {
         if (event.touches.length > 1) {
             event.preventDefault();
@@ -71,34 +72,43 @@ $(function () {
     $(document).on("click", ".sky-editor-img", function () {
         $("#sky-editor-file").click();
     })
-	$(document).on("click", ".sky-editor-video", function () {
-		$("#sky-editor-file-video").click();
-	})
-	$(document).on("change", "#sky-editor-file-video", function () {
-	        skyeditor.skyUpload("sky-editor-file-video", skyeditor.uploadVideo, function (e) {
-	            console.log(e.target.responseText);
-	            var res = JSON.parse(e.target.responseText);
-	
-	            if (res.error) {
-	                mui.toast(res.error);
-	            } else {
-	                var html = '<video controls  style="max-width:100%;" src="' + res.trueimgurl + '"></video>';
-	                skyEl.append(html);
-	            }
-	        })
-	    })
-    $(document).on("change", "#sky-editor-file", function () {
-        skyeditor.skyUpload("sky-editor-file", skyeditor.uploadUrl, function (e) {
+    $(document).on("click", ".sky-editor-video", function () {
+        $("#sky-editor-file-video").click();
+    })
+    $(document).on("change", "#sky-editor-file-video", function () {
+        skyeditor.skyUpload("sky-editor-file-video", skyeditor.uploadVideo, function (e) {
             console.log(e.target.responseText);
             var res = JSON.parse(e.target.responseText);
 
             if (res.error) {
                 mui.toast(res.error);
             } else {
-                var html = '<img src="' + res.trueimgurl + '"> &nbsp;';
+                var html = '<video controls  style="max-width:100%;" src="' + res.trueimgurl +
+                    '"></video>';
                 skyEl.append(html);
             }
         })
+    })
+
+    $(document).on('change', '#sky-editor-file',function () {
+        var that = this;
+		$(".sky-editor-loading").show();
+        lrz(that.files[0], {
+                width: 1024
+            })
+            .then(function (rst) {
+                $.post("/shop.php?m=upload&a=base64", {
+                    content: rst.base64
+                }, function (data) {
+					$(".sky-editor-loading").hide();
+                    if (data.error == 0) {
+                        var html = '<img src="' + data.trueimgurl + '"> &nbsp;';
+                        skyEl.append(html);
+                    }
+					
+                }, "json");
+            })
+
     })
     $(document).on("click", ".sky-editor-content", function () {
         if ($(".sky-editor-content div").length == 0) {
@@ -109,12 +119,25 @@ $(function () {
     })
     $(document).on("click", ".sky-editor-content div", function (e) {
         e.stopPropagation();
-		skyEl = $(this);
+        skyEl = $(this);
         var color = $(".sky-editor-color").css("background-color");
         if ($(this).css("color") != color) {
             $(this).css("color", color);
         }
-       
+    })
+    $(document).on("keyup", ".sky-editor-content", function (event) {
+        console.log(event.keyCode);
+        if (event.keyCode == "13"){     	 
+        	setTimeout(function(){
+        		if(skyEl.next().length>0){
+        			skyEl = skyEl.next();
+        			console.log(skyEl);
+        		}	
+        	},60) 	
+        	return false;
+        }
+        
+        
     })
 
     $(document).on("click", '.sky-editor-excute', function (e) {
@@ -123,11 +146,19 @@ $(function () {
         switch ($(this).data('role')) {
 
             case 'h3':
-               if($(this).hasClass("active")){
-               	skyEl.css({fontSize:"16px",fontWeight:"600","padding":"8px 0px"});
-               }else{
-               	skyEl.css({fontSize:"14px",fontWeight:"400","padding":"0"});
-               }
+                if ($(this).hasClass("active")) {
+                    skyEl.css({
+                        fontSize: "16px",
+                        fontWeight: "600",
+                        "padding": "8px 0px"
+                    });
+                } else {
+                    skyEl.css({
+                        fontSize: "14px",
+                        fontWeight: "400",
+                        "padding": "0"
+                    });
+                }
 
                 break;
             case "bold":
